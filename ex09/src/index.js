620a076c7ed5deae2f86d176c5ecfbe48a0a3559
opajkovic <oljaolja170@gmail.com>
2022-04-04 17:41:57 +0200

    added ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function randomInteger() {
+    return parseInt((Math.floor(Math.random() * 30)).toFixed(0));
+}
+
+
+console.log(randomInteger());
+
+module.exports = randomInteger;
+console.log(randomInteger());
+module.exports = randomInteger;
