d94e52778e75d457509b48a3cc5bc7be835f63d9
opajkovic <oljaolja170@gmail.com>
2022-04-04 18:10:58 +0200

    added ex11

---
+++ ex11/src/index.js
@@ -0,0 +1,7 @@
+function convertToInt(str) {
+    return parseInt(str, 16);
+}
+console.log(typeof(convertToInt("BA")));
+console.log(convertToInt("BA"));
+console.log(convertToInt("F1"));
+console.log(convertToInt("JeffBezos"));
