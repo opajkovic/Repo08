1fa7192e4515c8697c1a81e0d8f9c41b09d82aab
opajkovic <oljaolja170@gmail.com>
2022-04-05 17:43:58 +0200

    added

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -5,4 +5,8 @@ console.log(typeof(convertToInt("BA")));
 console.log(convertToInt("BA"));
 console.log(convertToInt("F1"));
 console.log(convertToInt("JeffBezos"));
+typeof(convertToInt("BA"));
+convertToInt("BA");
+convertToInt("F1");
+convertToInt("JeffBezos");
 module.exports = convertToInt;
